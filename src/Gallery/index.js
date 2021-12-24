5fd70c0de7b98bf43b2d1b0a46c9f621a8aa2018
Thomas Walker <thomasnwalker@yahoo.com>
2021-12-23 19:18:46 -0800

    verticalized photo gallery

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -7,43 +7,50 @@ function Gallery(){
         
         <div>
             <Row>
+
                 <Col>
                     <img className="gallery-photos-type1" src={require(`../assets/8.jpeg`)} alt="model-duo"/>
                 </Col>
+                
+            </Row>
+            <Row>
 
                 <Col>
                     <img className="gallery-photos-type1" src={require(`../assets/3.jpeg`)} alt="featured-models"/>
                 </Col>
+
             </Row>
 
             <Row>
-                <Col></Col>
 
                 <Col>
                     <img className="gallery-photos-type1" src={require(`../assets/7.jpeg`)} alt="all-white"/>
                 </Col>
 
-                <Col></Col>
             </Row>
 
             <Row>
+
                 <Col> 
                 <img className="gallery-photos-type1" src={require(`../assets/9.jpeg`)} alt="yoshi"/>
-
                 </Col>
+                
+            </Row>
+
+            <Row>
 
                 <Col>
                 <img className="gallery-photos-type1" src={require(`../assets/6.jpeg`)} alt="jaga"/>
-
                 </Col>
+
             </Row>
             <Row>
-                <Col></Col>
+
                 <Col>
                     <img className="gallery-photos-type1" src={require(`../assets/4.jpeg`)} alt="jaga"/>
 
                 </Col>
-                <Col></Col>
+
             </Row>
             
         </div>
