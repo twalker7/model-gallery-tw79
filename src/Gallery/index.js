3304f635352e7456e5e670e54e623a5a7237b35d
Thomas Walker <thomasnwalker@yahoo.com>
2021-12-23 16:52:01 -0800

    styling gallery

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -8,12 +8,22 @@ function Gallery(){
         <div>
             <Row>
                 <Col>
-                    <img src={require(`../assets/4.jpeg`)} alt="jaga"/>
-                </Col>
-                <Col>
-                    
+                    <img className="gallery-photos-type1" src={require(`../assets/4.jpeg`)} alt="jaga"/>
                 </Col>
 
+                <Col>
+                    <img className="gallery-photos-type1" src={require(`../assets/3.jpeg`)} alt="jaga"/>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col></Col>
+
+                <Col>
+                    <img className="gallery-photos-type1" src={require(`../assets/9.jpeg`)} alt="jaga"/>
+                </Col>
+
+                <Col></Col>
             </Row>
             
         </div>
