0c300b803439c5effc0c84a4d7acfc79a783a486
Thomas Walker <thomasnwalker@yahoo.com>
2021-12-24 01:19:59 -0800

    added email link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1> Thomas Walker </h1>
+       <a className="component-header" target="_blank" rel="noreferrer" href="mailto: twalker@onetechadvanced.com"> Email Me </a>
+
       </header>
 
       <Gallery> </Gallery>
